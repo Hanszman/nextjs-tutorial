6b0eaad1a7ee44bf5e124c7d34727103cf3ebc27
Hanszman <victor.hanszman@hotmail.com>
2025-10-29 20:32:02 -0300

    exploring even more features from next on client and server components

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -16,10 +16,49 @@ export default async function Posts() {
     const data: ResponseProps = await response.json();
     console.log('DATA:', data);
 
+    async function handleFetchPosts() {
+        'use server'
+        console.log('CLICOOOU');
+    }
+
+    async function handleSearchUsers(formData: FormData) {
+        'use server'
+        const userId = formData.get('userId');
+        console.log('TESTEEEE', userId);
+    }
+
     return (
         <div>
             <h1  className="text-center mt-5 mb-2 font-bold text-3xl">Todos os posts!</h1>
+            
+            <button
+                className="bg-zinc-900 text-white rounded-md px-2 py-1 m-5 cursor-pointer"
+                onClick={handleFetchPosts}
+                type="button"
+            >
+                Buscar Posts
+            </button>
             <Button/>
+
+            <form
+                className="flex fap-2 my-4"
+                action={handleSearchUsers}
+            >
+                <input
+                    type="text"
+                    placeholder="ID do Usuário"
+                    className="border border-gray-200 p-2"
+                    name="userId"
+                />
+                <button
+                    className="bg-blue-500 text-white p-2 cursor-pointer"
+                    onClick={handleFetchPosts}
+                    type="submit"
+                >
+                    Buscar Usuário
+                </button>
+            </form>
+
             <div className="flex flex-col gap-4 mx-2">
                 {data.posts.map(post => (
                     <div key={post.id} className="bg-gray-200 p-4 rounded-md">
