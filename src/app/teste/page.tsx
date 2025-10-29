dc33ce35afbf024cafb759e8c9e5d8bf76b60c2e
Hanszman <victor.hanszman@hotmail.com>
2025-10-29 20:09:11 -0300

    learning difference between server and client components

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { useEffect, useState } from "react";
+
+export default function PageTeste() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        console.log('USE EFFECT')
+        fetch('https://dummyjson.com/posts')
+            .then(res => res.json())
+            .then(data => setPosts(data.posts));
+    }, [])
+
+    return (
+        <div>
+            <h1  className="text-center mt-5 mb-2 font-bold text-3xl">Página Cliente!</h1>
+            <button className="bg-zinc-900 text-white rounded-md px-2 py-1 m-5 cursor-pointer" onClick={() => alert('TESTE!!!')}>
+                TESTE
+            </button>
+            <div className="flex flex-col gap-4 mx-2">
+                {posts.map((post: any) => (
+                    <div key={post.id} className="bg-gray-200 p-4 rounded-md">
+                        <h2 className="font-bold">{post.title}</h2>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
