1d4af427b1b78dc3a1e832f951c2f73eb6cd7055
Hanszman <victor.hanszman@hotmail.com>
2025-10-29 21:16:59 -0300

    suspense fallback feature used as loading

---
+++ src/app/posts/[id]/_loading.tsx
@@ -0,0 +1,4 @@
+// Renaming this component only to "loading.tsx", without the "_" it is going to be called automatically when the page is waiting for something
+export default function Loading() {
+    return <h1>Carregando...</h1>;
+}
--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,19 +1,17 @@
-import { PostProps } from "../page";
+import { Suspense } from "react";
+import { PostInfo } from "./_components/post";
 
 export default async function DetailPost({params}: {
     params: Promise<{id: string}>
 }) {
     const { id } = await params;
-    await new Promise(resolve => setTimeout(resolve, 4000));
-    const response = await fetch(`https://dummyjson.com/posts/${id}`)
-    const data: PostProps = await response.json();
-    console.log('DATA:', data);
 
     return (
         <div>
-            <h1>Detalhes do Post: {id}</h1>
-            <h2>{data.title}</h2>
-            <p>{data.body}</p>
+            <h1 className="text-2xl font-bold text-center">Detalhes do Post: {id}</h1>
+            <Suspense fallback={<h1>Carregando...</h1>}>
+                <PostInfo id={id}/>
+            </Suspense>
         </div>
     );
 }
